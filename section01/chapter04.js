ac189ecf930be4f492e6b18119fa0afbef201d39
yeen322 <yemonmon321@gmail.com>
2025-09-10 21:31:27 +0900

    1.4 변수와 상수

---
+++ section01/chapter04.js
@@ -0,0 +1,19 @@
+//1. 변수
+let age = 22;
+age =23;
+
+//2. 상수
+const birthday="2004.03.22";
+
+//3. 변수 명명 규칙
+//3-1. $,_ 제외한 기호 사용 x
+let $_name;
+
+//3-2. 숫자로 시작할 수 없음
+let name1; // let 2name;
+
+//3-3. 예약어 사용x
+// let let;
+
+//3. 변수 명명 가이드
+let isTrueOrFalse;
