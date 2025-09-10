21ce2be5800278aa311c90da8433970d632abc14
yeen322 <yemonmon321@gmail.com>
2025-09-10 21:43:06 +0900

    1.5 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,27 @@
+//1. number type
+let num1=2;
+let num2=1.2;
+let num3=-1;
+
+let inf = Infinity;
+let iInf = -Infinity;
+
+let nan=NaN;
+
+//2. String type
+let myName='yeeun';
+let myLocation='Dajeon';
+let introduce=myName+myLocation;
+
+let introduceText =`${myName}은 ${myLocation}에 거주`;
+
+//3. boolean type
+let isSwitchOn = true;
+let isEmpty=false;
+
+//4. Null type(의도적,값이 없음)
+let empty = null;
+
+//5. Undefined type(자동 할당)
+let none;
+
