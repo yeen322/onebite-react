62bbfd23694a6fd32bd2493c7097c4e22d91ad2b
yeen322 <yemonmon321@gmail.com>
2025-09-11 14:15:32 +0900

    연산자1

---
+++ section01/chapter07.js
@@ -0,0 +1,40 @@
+//1. 대입 연산자
+let var1 = 1;
+
+//2. 산술 연산자
+let num1=3+1;
+let num2=3-1;
+let num3=3*1;
+let num4=3/1;
+let num5=3%1;
+
+//3. 복합 대입 연산자
+let num6=10;
+num6+=20; // num6 = num6+20;
+num6-=20;
+num6*=20;
+num6/=20;
+num6%=20;
+
+//4. 증감 연산자
+let num7 =1;
+num7++;
+++num7; // 후위
+num7--; // 전위
+--num7;
+
+//5. 논리 연산자
+let or = ture || false;
+let and = true && false;
+let not = !true;
+
+//6. 비교 연산자
+let comp1 = 1 === "1"; //자료형+값
+let comp2 = 1 !== 2; // 값
+
+// 대소 비교
+let comp3 = 2 > 1;
+let comp4 = 2 < 1;
+
+let comp5 = 2 >= 2;
+let comp6 = 2 <= 2;
