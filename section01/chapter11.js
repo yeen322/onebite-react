333547ac578e2ba3ac00eefb864ec7023a00cc64
yeen322 <yemonmon321@gmail.com>
2025-09-17 11:41:07 +0900

    1-11. 함수

---
+++ section01/chapter11.js
@@ -0,0 +1,13 @@
+//함수 선언
+// function greeting() {
+//   console.log("안녕하세요!");
+// }
+// greeting();
+
+function getArea(width, height) {
+  // 매개변수
+  let area = width * height;
+
+  return area;
+}
+getArea(10, 29); // 인수
